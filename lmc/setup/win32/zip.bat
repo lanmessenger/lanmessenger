f171fe2f8add0e5867c392e4cfb951beeaf83931
lanmessenger <lanmsgnr@gmail.com>
2018-12-24 18:12:37 +0300

    Add .ps1 script for zip

---
+++ lmc/setup/win32/zip.bat
@@ -0,0 +1,1 @@
+Powershell.exe -ExecutionPolicy Bypass -File zip.ps1
---
+++ lmc/setup/win32/zip.ps1
@@ -0,0 +1,18 @@
+﻿#
+# Script.ps1
+#
+
+$exePath = '..\..\build-lmc-Desktop_Qt_5_11_2_MSVC2017_32bit_Static_3-Release'
+
+Compress-Archive `
+	-LiteralPath $exePath\lmc.exe, `
+				$exePath\libeay32.dll, `
+				$exePath\ssleay32.dll, `
+				$exePath\sounds, `
+				$exePath\lang, `
+				..\..\src\resources\text\license.txt, `
+				..\..\src\resources\text\readme.txt `
+	-CompressionLevel Optimal `
+	-Force `
+	-DestinationPath ..\lmc-1.2.39-win32.zip
+
